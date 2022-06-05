7af8f048aa29038d0bf0afec8fa8a41bf7f64986
Koshelev13 <100027286+Koshelev13@users.noreply.github.com>
2022-06-06 02:31:33 +0300

    Update newton.m

--- Matlab/newton.m
+++ Matlab/newton.m
@@ -2,7 +2,7 @@ clear
 a=0;
 b=14;
 x = linspace(a,b,500);
-h = 0.001; iter = 100; eps = 0.001;
+h = 0.001; iter = 1000; eps = 0.001;
 %f = @(x)1+(1+sin(x)-cos(x)).^2-(sin(2*x)-cos(2*x)-0.2).^2;
 f = @(x)sin(x)./x;
 y = f(x);
