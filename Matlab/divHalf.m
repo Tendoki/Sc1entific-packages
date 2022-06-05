26ebb8e7cab6661fa3a9da9ab72de847f79cd045
Koshelev13 <100027286+Koshelev13@users.noreply.github.com>
2022-06-06 02:32:02 +0300

    Update divHalf.m

--- Matlab/divHalf.m
+++ Matlab/divHalf.m
@@ -1,7 +1,7 @@
 clear
 a = 0; b = 14;
 x = linspace(a,b,500)
-h = 0.001; iter=100; eps=0.001;
+h = 0.001; iter=1000; eps=0.001;
 f=@(x)sin(x)./x;
 y=f(x);
 plot(x,f(x),x,0*x,':'); grid on
@@ -14,7 +14,7 @@ axis([a,b,ymin,ymax]);
 z = ginput(2); z1=z(1,1), z2=z(2,1);
 f1 = f(z1); f2=f(z2); z=(z1+z2)/2; y=f(z);
 P = plot(z1,0,'*',z2,0,'*',z,0,'o');
-if f1*f2>0 'Bad points'
+if f1*f2>0 'Плохие точки'
 end;
 for i=1:iter
     z=(z1+z2)/2; y=f(z);
@@ -28,4 +28,5 @@ for i=1:iter
         break;
     end;
 end;
+disp("Найденный корень " + z);
 hold off
